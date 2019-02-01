488df5762fd82a1fffa779ff9b9cfdfdae3723fd
Cybersecurity Team at National university (NUCL3@R) <37946208+CentralRepo1@users.noreply.github.com>
2019-02-01 00:03:31 -0500

    Update croncheck.sh

--- scripts/croncheck.sh
+++ scripts/croncheck.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 # Checks all running cron jobs for all users on local machine
+# cat is used to print the file, tr replaces all colons with a space
+# and awk is used to print only the first field $1, which is the 
+# username. crontab must have sudo prefixed to show that data.
 
-for user in $(cut -f1 -d: /etc/passwd); do crontab -u $user -l; done
+
+for username in $(cat /etc/passwd | tr ":" " " | awk '[print $1 }'); 
+do 
+  sudo crontab -u $username -l; 
+done
